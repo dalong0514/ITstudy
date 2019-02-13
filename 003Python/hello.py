41f3867b0c6e42a8b91a2c6dd61449ca877e61d5
大龙 <dalong0514@me.com>
2019-02-13 17:45:30 +0800

    20190209

--- 003Python/hello.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python3
-# -*- coding: utf-8 -*-
-import os
-import re
-data='Thu Feb 15 17:46:04 2007::uzifzf@dpyivihw.gov::1171590364-6-8'
-s='spam'
-print(s.__add__('dalong'))
-
-print(s)
--- 003Python/script1.py
+++
@@ -1,5 +0,0 @@
-import sys
-print(sys.platform)
-print(2**100)
-x='Spam!'
-print(x*8)
