274dec954693b69156dd27a42d206c3a42fe04b5
dalong3 <dalong0514@126.com>
2020-02-09 23:16:40 +0800

    20200209

---
+++ coursecode/2020006cy_data_analysis_code/sourcedata.py
@@ -0,0 +1,9 @@
+def dict_01():
+    return {'Chinese': [66, 95, 93, 90,80],'English': [65, 85, 92, 88, 90],
+    'Math': [30, 98, 96, 77, 90]}
+
+def list_01():
+    return ['ZhangFei', 'GuanYu', 'ZhaoYun', 'HuangZhong', 'DianWei']
+
+def list_02():
+    return ['English', 'Math', 'Chinese']
